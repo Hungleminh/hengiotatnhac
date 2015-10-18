0c5d7b2f32fa00e11f619e9016ddb6d6cf7d0300
Hung <hung@hungs-mac-mini.local>
2015-10-18 16:58:21 +0700

    Initial Commit

---
+++ HenGioTatNhac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HenGioTatNhac
+//
+//  Created by Hung on 10/18/15.
+//  Copyright © 2015 H-Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
